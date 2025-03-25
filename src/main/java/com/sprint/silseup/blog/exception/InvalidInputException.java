e27ff8dee6f9f071c2e0d1279fafe43b59b9ff7e
sangyeobhan <sangyeobhandev@gmail.com>
2025-03-25 09:33:21 +0900

    feat: Custom Exception 추가

---
+++ src/main/java/com/sprint/silseup/blog/exception/InvalidInputException.java
@@ -0,0 +1,7 @@
+package com.sprint.silseup.blog.exception;
+
+public class InvalidInputException extends RuntimeException {
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
